c0cc93652c29e19ed1370c6b20349130882d6620
Dmitriy <zamulin1998@gmail.com>
2021-11-17 21:00:52 +0300

    Create a couple of functions, now the converter works stable and properly. Language switch function was added

--- script.js
+++ script.js
@@ -5,82 +5,148 @@ const inputTo = document.querySelector('#currency-to');
 const selectBoxes = document.querySelectorAll('.select-box');
 const convertButton = document.querySelector('#convert');
 const amount = document.querySelector('#amount');
-const apiKey = '96705bd0-417f-11ec-8d6c-45c8b69d2b4f';
-const mainUrl = `https://freecurrencyapi.net/api/v2/latest?apikey=${apiKey}`;
+const apiKey = '914d1b78d615771f93ba';
+const mainUrl = `https://free.currconv.com`;
+const listOfCurrenciesURL = mainUrl + `/api/v7/currencies?apiKey=${apiKey}`;
+const buttonSwap = document.querySelector('.icon');
 const select = document.querySelectorAll('select');
+const output = document.querySelector('#output');
+const ruLang = {
+   title: 'Конвертер валют',
+   amountTitle: 'Введите значение',
+   selectText: 'Введите число',
+   from: 'Конвертировать из',
+   to: 'В',
+   button: 'Конвертировать'
+};
+const enLang = {
+   title: 'Currency Converter',
+   amountTitle: 'Enter amount',
+   selectText: 'Input number',
+   from: 'From',
+   to: 'To',
+   button: 'Convert'
+};
 
-async function request() {
-   const response = await fetch(mainUrl);
+async function request(url) {
+   const response = await fetch(url);
    const data = await response.json();
-   console.log(data)
-   await setInfo(data)
+   return await data;
 }
-request();
 
 function setInfo(response) {
-   const data = response.data;
+   const data = response.results;
    for (let i = 0; i < select.length; i++) {
       for (let currency in data) {
          let selected;
-         for (let i = 0; i < select.length; i++) {
-            if (i == 0) {
-               selected = currency == 'USD' ? 'selected' : '';
-               //console.log(selected)
-            } else if (i == 1) {
-               selected = currency == 'RUB' ? 'selected' : '';
-               console.log(selected)
-            }
+
+         if (i == 0) {
+            selected = currency == 'RUB' ? 'selected' : '';
+         } else if (i == 1) {
+            selected = currency == 'USD' ? 'selected' : '';
          }
-         const option = `<option value="${data[currency]}" label="${currency}"${selected}>${currency}</option>`;
+
+         const option = `<option value="${currency}"${selected}>${currency}</option>`;
          select[i].insertAdjacentHTML('beforeend', option);
       }
    }
-   //for (let currency in data) {
-   //   let selected;
-   //   for (let i = 0; i < select.length; i++) {
-   //      if (i == 0) {
-   //         selected = currency == 'USD' ? 'selected' : '';
-   //      } else if (i == 1) {
-   //         selected = currency == 'RUB' ? 'selected' : '';
-   //      }
-   //   }
-   //   select.forEach((element) => {
-   //      if (element == 'RUB' || element == 'USD') {
-   //         element.innerHTML += `<option value="${data[currency]}" label="${currency}" selected>${currency}</option>`;
-   //      } else {
-   //         element.innerHTML += `<option value="${data[currency]}" label="${currency}">${currency}</option>`;
-   //      }
-   //   });
-   //}
-   //selectBoxes.forEach((box) => {
-   //   const currency = box.querySelector('select');
-   //   let currencyOption = currency.querySelector('option')
-   //   console.log(currencyOption.label)
-   //   //const flag = currency.slice(0, -1);
-   //   //const img = box.querySelector('img');
-   //   //img.src = `http://purecatamphetamine.github.io/country-flag-icons/3x2/${flag}.svg`;
-   //})
+   select.forEach((element) => {
+      const flag = element.value.slice(0, -1);
+      const img = element.parentNode.querySelector('img');
+      img.src = `http://purecatamphetamine.github.io/country-flag-icons/3x2/${flag}.svg`;
+   });
 }
+
+async function start() {
+   const response = await request(listOfCurrenciesURL);
+   await setInfo(response);
+}
+start();
+
 function setFlag() {
-   const currency = this.querySelector('select').label;
+   const currency = this.querySelector('select').value;
    const flag = currency.slice(0, -1);
    const img = this.querySelector('img');
    img.src = `http://purecatamphetamine.github.io/country-flag-icons/3x2/${flag}.svg`;
 }
 
 selectBoxes.forEach((element) => {
-   element.addEventListener('click', setFlag)
+   element.addEventListener('click', setFlag);
 });
 
-//function convert() {
-//   //let amount = inputFrom.value;
-//   let course = inputFrom.value
-//   if (amount === '' || amount === 0) {
-//      inputFrom.value = 1;
-//      amount = 1;
-//   }
-//   console.log(course)
-//   //let result = 
-//}
+function swapCurrency() {
+   const currentValue = inputFrom.value;
+   inputFrom.value = inputTo.value;
+   inputTo.value = currentValue;
+}
+
+buttonSwap.addEventListener('click', swapCurrency);
+
+async function convert() {
+   let value = amount.value;
+   value = value.replace(/\,/, '.');
+   const from = inputFrom.value;
+   const to = inputTo.value;
+   if (output.classList.contains('error')) {
+      output.classList.remove('error');
+      amount.classList.remove('error');
+   }
+   if (/[a-z]/i.test(value)) {
+      output.textContent = `Введите корректное значение !`;
+      output.classList.add('error');
+      amount.classList.add('error');
+      return false;
+   }
+
+   if (value === '' || value === 0) {
+      value = 1;
+   }
+
+   const requestUrl = mainUrl + `/api/v7/convert?q=${from}_${to}&compact=ultra&apiKey=${apiKey}`;
+   const response = await request(requestUrl);
+
+   for(let course in response) {
+      const result = (value * response[course]).toFixed(3);
+      output.textContent = `${value} ${from} = ${result} ${to}`;
+   }
+}
 
 convertButton.addEventListener('click', convert);
+
+function selectLanguage() {
+   if(event.target.classList.contains('language')) {
+      if (event.target.classList.contains('language--active')) {
+         return false;
+      }
+      
+      const lang = event.target.getAttribute('data-lang');
+      const headerTitle = document.querySelector('.converter__header');
+      const title = document.querySelector('.title-amount');
+      const amountInput = amount;
+      const from = document.querySelector('.headline--from');
+      const to = document.querySelector('.headline--to');
+      const button = convertButton;
+
+      document.querySelector('.language--active').classList.remove('language--active');
+      event.target.classList.add('language--active');
+
+      if (lang === 'RU') {
+         setLanguage(ruLang)
+      } else if (lang === 'EN') {
+         setLanguage(enLang);
+      } else {
+         alert('Error, this function works unproperly');
+      }
+
+      function setLanguage(language) {
+         headerTitle.textContent = language.title;
+         title.textContent = language.amountTitle;
+         amountInput.placeholder = language.selectText;
+         from.textContent = language.from;
+         to.textContent = language.to;
+         button.textContent = language.button;
+      }
+   }
+}
+
+document.querySelector('.converter').addEventListener('click', selectLanguage)
